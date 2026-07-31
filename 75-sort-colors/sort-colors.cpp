9611d6599a825b13069e0db6bbd981e4ada2111d
DevanshSingh <04devanshsingh07@gmail.com>
2026-07-31 07:53:07 +0530

    Enhance comments for sortColors function
    Added comments to clarify the Dutch National Flag Algorithm implementation.

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        
         int n = nums.size();
         int low = 0, mid = 0, high  = n-1;
 
+        // Dutch National Flag Algorithm.
+
+        // Both mid and low will be pointing Arrays/vectors first element and high will point last element.
+
+        // Points to Remember: 
+
+        //  0's -> 0 to low-1
+        // 1's -> low to mid-1
+        // 2's -> high+1 to n-1
+
+        // Unsorted part -> mid to high 
+
         while(mid <= high)
         {
-            if(nums[mid] == 0)
+            if(nums[mid] == 0)  // When 0 is present at the mid.
             {
                 swap(nums[low], nums[mid]);
                 mid++;
                 low++;
             }
-            else if(nums[mid] == 1)
+            else if(nums[mid] == 1)  // When 1 is present at the mid.
             {
                 mid++;
             }
             else 
             {
-                swap(nums[high], nums[mid]);
+                swap(nums[high], nums[mid]);  // When 2 is present at mid.
                 high--;
             }
         }
