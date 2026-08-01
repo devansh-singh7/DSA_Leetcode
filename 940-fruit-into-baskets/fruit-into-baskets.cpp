d02638f34aa77fe3546a94124043a39f5ca3cda6
DevanshSingh <04devanshsingh07@gmail.com>
2026-08-01 08:26:36 +0530

    Time: 34 ms (83.79%) | Memory: 84.9 MB (34.40%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int low = 0;
+        unordered_map <int, int> f;
+        int res = INT_MIN;
+
+        // Increasing Window size
+        for(int high=0; high<n; high++)
+        {
+            f[fruits[high]]++;
+
+            while(f.size() > 2)  // Atmost 2 is asked so till 2.
+            {
+                f[fruits[low]]--;   // Shrinking window
+
+                // Edge Case 
+                if(f[fruits[low]] == 0) // Erase 0 frequency variable.
+                {
+                    f.erase(fruits[low]);
+                }
+                low++;
+            }
+            if(f.size() == 2 || f.size() < 2)  // ATmost 2 so, equal and less than 2.
+            {
+                int len = high - low + 1;
+                res = max(res, len);
+            }
+
+        }
+        return res == INT_MIN ? 0 : res;
+    }
+};
