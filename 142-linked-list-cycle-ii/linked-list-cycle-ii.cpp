701de4e21f0fe0b6e4f8b788ce118edd351f600c
DevanshSingh <04devanshsingh07@gmail.com>
2026-08-05 08:29:43 +0530

    Enhance comments in detectCycle function
    Added comments to clarify the slow-fast pointer approach and edge case handling.

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,16 +9,19 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(!head || !head->next) return NULL;
 
-        ListNode *slow = head;
+        // Slow - Fast Pointer Pattern
+        
+        if(!head || !head->next) return NULL; // Edge case
+
+        ListNode *slow = head; 
         ListNode *fast = head;
 
         // 1. Traverse using a WHILE loop until fast reaches the end or a cycle is detected.
         while(fast != NULL && fast->next != NULL)
         {
-            slow = slow->next;
-            fast = fast->next->next;
+            slow = slow->next; // 1 step move.
+            fast = fast->next->next; // 2 step move.
 
             // 2. Cycle Found
             if(slow == fast)
@@ -27,6 +30,7 @@ public:
                 while(slow != fast) {
                     slow = slow->next;
                     fast = fast->next;
+                    // Meeting point of fast and slow here gives the node from which cycle has started.
                 }
                 return slow; // The start of the cycle
             }
